d820bb6b2d1819050a3546a36b9103760010b14b
SeonJungKim <ksj083895@gmail.com>
2021-07-15 10:56:57 +0900

    현상금 리스트 생성

---
+++ BountyList/BountyViewController.swift
@@ -0,0 +1,59 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by Seogun Kim on 2021/07/14.
+//
+
+import UIKit
+
+
+
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // list cell을 사용하기위해서 캐스팅(as?)을 사용
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+        return cell
+        
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+//            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+//            cell.imgView.image = img
+//            cell.nameLabel.text = nameList[indexPath.row]
+//            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+//            return cell
+//        } else {
+//            return UITableViewCell()
+//        }
+}
+    
+    // UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("---> \(indexPath.row)")
+    }
+}
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel : UILabel!
+}
