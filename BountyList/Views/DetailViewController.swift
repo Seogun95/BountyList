b990a40ea47afd3eee12d1dc4d0a6f8b722f8753
SeonJungKim <ksj083895@gmail.com>
2021-07-21 16:33:19 +0900

    뷰 속성을 이용한 애니메이션 효과 적용

--- BountyList/Views/DetailViewController.swift
+++ BountyList/Views/DetailViewController.swift
@@ -12,19 +12,19 @@ class DetailViewController: UIViewController {
     // MVVM
     
     /*
-    // Model
-    // - bountyInfo
-    // > bountyInfo 만들기
-    
-    // View
-    // - imgView, nameLabel, bountyLabel
-    // > view들은 viewModel을 통해 구성됨
-    
-    // ViewModel
-    // - DetailViewModel
-    // > 뷰레이어에서 필요한 메소드 만들기
-    // > Model 가지고 있기 (bountyInfo)
-*/
+     // Model
+     // - bountyInfo
+     // > bountyInfo 만들기
+     
+     // View
+     // - imgView, nameLabel, bountyLabel
+     // > view들은 viewModel을 통해 구성됨
+     
+     // ViewModel
+     // - DetailViewModel
+     // > 뷰레이어에서 필요한 메소드 만들기
+     // > Model 가지고 있기 (bountyInfo)
+     */
     
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -33,7 +33,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var BountyLabelCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
@@ -46,23 +46,39 @@ class DetailViewController: UIViewController {
     }
     
     private func prepareAnimation() {
-        NameLabelCenterX.constant = view.bounds.width
-        BountyLabelCenterX.constant = view.bounds.width
+        //        NameLabelCenterX.constant = view.bounds.width
+        //        BountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).rotated(by: 180).scaledBy(x: 3, y: 3)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).rotated(by: 180).scaledBy(x: 3, y: 3)
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     private func showAnimation() {
-        NameLabelCenterX.constant = 0
-        BountyLabelCenterX.constant = 0
+        //        NameLabelCenterX.constant = 0
+        //        BountyLabelCenterX.constant = 0
+        //
+        //        // 레이어 애니메이션 효과
+        //        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .allowUserInteraction, animations: {
+        //            self.view.layoutIfNeeded()
+        //        }, completion: nil)
+        //
+        //        // 이미지 뷰 애니메이션 효과
+        //        UIView.transition(with: imgView, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         
-        // 레이어 애니메이션 효과
-        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .allowUserInteraction, animations: {
-            self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
         }, completion: nil)
         
-        // 이미지 뷰 애니메이션 효과
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.7, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
+        
         UIView.transition(with: imgView, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
-    
     
     func updateUI() {
         if let bountyInfo = viewModel.BountyInfo {
@@ -71,7 +87,7 @@ class DetailViewController: UIViewController {
             bountyLabel.text = "\(bountyInfo.bounty)"
         }
     }
-
+    
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
